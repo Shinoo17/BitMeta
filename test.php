1fa05ac343459c7b5ea49579bfe9c0b38e37d243
Seres <o0peetarza0o@gmail.com>
2021-10-21 15:45:15 +0700

    comment

---
+++ test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>imgForm</title>
+    <meta charset="UTF-8">
+    
+</head>
+<body>
+    <center style="margin-top: 10%;">
+        <form class="imgForm" action="profile.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="upload" />
+            <input type="submit"  name="save"  value="upload" />
+        </form>
+    </center>
+</body>
+</html>
